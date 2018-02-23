0ca55df7afb2b07c098ba1d7f777eefd625c9837
Curtis <Curtis@MacBook-Pro-de-Alejandro.local>
2018-02-23 17:54:21 +0100

    Temp file test function

--- src/common/__tests__/catastro.utils.test.ts
+++ src/common/__tests__/catastro.utils.test.ts
@@ -15,7 +15,8 @@ const BOADILLA = fs.readFileSync(path.resolve(__dirname, 'boadilla.xml'), 'utf-8
 const INVALID = fs.readFileSync(path.resolve(__dirname, 'invalid.xml'), 'utf-8');
 
 const DNPMostoles = fs.readFileSync(path.resolve(__dirname, 'DNPmostoles.xml'), 'utf-8');
-
+const tempFolder = path.resolve(process.cwd(), 'temp');
+const tempFilePath = (fileName: string) => path.resolve(tempFolder, fileName + '.xml');
 /**
  * Read files inside a temp folder, due to batch-xml-processing testing
  *
@@ -45,24 +46,28 @@ describe('Catastro Parsers utilities', () => {
   });
 
   test('Get DNP existant', () => {
-    expect(consultaDNPRBodyParser(DNPMostoles)).toEqual(JSON.parse(
-      '{"cn":"UR","pc1":"1111111","pc2":"1111111","car":"0001","cc1":"O","cc2":"D","cm":"92","cp":"28","cmc":"92","np":"MADRID","nm":"MOSTOLES","dp":"28888","dm":"4","ldt":"CL PRUEBA 7 28888 MOSTOLES (MADRID)","dir":{"cv":"921","tv":"CL","nv":"PRUEBA","pnp":"7"},"loint":{"es":"T","pt":"OD","pu":"OS"},"debi":{"luso":"Residencial","sfc":"204","cpt":"100,000000","ant":"1998"},"lcons":[{"lcd":"VIVIENDA","es":"1","pt":"00","pu":"01","stl":"56","lsrp":[]},{"lcd":"VIVIENDA","es":"1","pt":"01","pu":"01","stl":"60","lsrp":[]},{"lcd":"VIVIENDA","es":"1","pt":"02","pu":"01","stl":"24","lsrp":[]},{"lcd":"APARCAMIENTO","es":"1","pt":"SM","pu":"01","stl":"64","lsrp":[]}]}'
-    ));
+    expect(consultaDNPRBodyParser(DNPMostoles)).toEqual(
+      JSON.parse(
+        '{"cn":"UR","pc1":"1111111","pc2":"1111111","car":"0001","cc1":"O","cc2":"D","cm":"92","cp":"28","cmc":"92","np":"MADRID","nm":"MOSTOLES","dp":"28888","dm":"4","ldt":"CL PRUEBA 7 28888 MOSTOLES (MADRID)","dir":{"cv":"921","tv":"CL","nv":"PRUEBA","pnp":"7"},"loint":{"es":"T","pt":"OD","pu":"OS"},"debi":{"luso":"Residencial","sfc":"204","cpt":"100,000000","ant":"1998"},"lcons":[{"lcd":"VIVIENDA","es":"1","pt":"00","pu":"01","stl":"56","lsrp":[]},{"lcd":"VIVIENDA","es":"1","pt":"01","pu":"01","stl":"60","lsrp":[]},{"lcd":"VIVIENDA","es":"1","pt":"02","pu":"01","stl":"24","lsrp":[]},{"lcd":"APARCAMIENTO","es":"1","pt":"SM","pu":"01","stl":"64","lsrp":[]}]}'
+      )
+    );
   });
 
   test('Has test files', () => {
-    expect(readFilesFromContainer().length).toBeGreaterThanOrEqual(1)
+    expect(readFilesFromContainer().length).toBeGreaterThanOrEqual(1);
   });
 
   test.only('Get DNP Existant from single file', () => {
-    const _file = 'D:\\Desarrollo\\certiWS\\temp\\108.xml'
+    const _file = tempFilePath('108');
     expect(consultaDNPRBodyParser(fs.readFileSync(_file, 'utf-8'))).not.toThrow();
   });
 
   test.skip('DNP batch-parsing not throwing any exception', () => {
-    expect(() => _filesFromContainer.map(item => {
-      console.log(item);
-      consultaDNPRBodyParser(fs.readFileSync(item, 'utf-8'));
-    })).not.toThrow();
+    expect(() =>
+      _filesFromContainer.map(item => {
+        console.log(item);
+        consultaDNPRBodyParser(fs.readFileSync(item, 'utf-8'));
+      })
+    ).not.toThrow();
   });
 });
